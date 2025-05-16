66b1e74b110ae3b0fa4c52a191cd2943d437f3e6
Andrii Ishchuk <a_ishchuk22@ukr.net>
2025-05-16 12:28:43 +0300

    added navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage/HomePage';
-import CatalogPage from './pages/CatalogPage/CatalogPage';
-import CarDetailsPage from './pages/CarDetailsPage/CarDetailsPage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import HomePage from "./pages/HomePage/HomePage";
+import CatalogPage from "./pages/CatalogPage/CatalogPage";
+import CarDetailsPage from "./pages/CarDetailsPage/CarDetailsPage";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/catalog" element={<CatalogPage />} />
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,31 @@
+import { NavLink } from "react-router-dom";
+import s from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <div className={s.header}>
+      <NavLink to="/" className={s.logo}>
+        <svg width="104" height="16">
+          <use href="/icons/symbol-definitions.svg#icon-RentalCar" />
+        </svg>
+      </NavLink>
+
+      <nav className={s.nav}>
+        <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? s.active : s.link)}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/catalog"
+          className={({ isActive }) => (isActive ? s.active : s.link)}
+        >
+          Catalog
+        </NavLink>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
